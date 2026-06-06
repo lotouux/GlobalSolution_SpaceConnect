dcf8b120a430cbbdf265a9020265b0652e08efef
lotouux <beatriz.c.serafini@gmail.com>
2026-06-05 23:10:37 -0300

    refactor: limpeza de código

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+export default [
   { ignores: ["dist", ".output", ".vinxi"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
@@ -24,4 +24,4 @@ export default tseslint.config(
     },
   },
   eslintPluginPrettier,
-);
+];
